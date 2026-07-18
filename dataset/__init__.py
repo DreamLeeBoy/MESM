4f49aeb2c9ed1546b85afab30559efeac5c7b316
DreamLeeBoy <83645774+DreamLeeBoy@users.noreply.github.com>
2026-07-18 10:50:19 +0800

    Use phrase-aware Charades dataset in original MESM loader

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,7 +1,9 @@
 from .base import BaseDataset
 from .base import SplitGatherBatchSampler
-from .base import collate, prepare_batch_input
-from .charades import CharadesDataset
+from .base import prepare_batch_input
+from .charades import PhraseContrastiveCharadesDataset as CharadesDataset
+from .charades import collate_phrase_contrastive as collate
+from .charades import configure_phrase_contrastive_dataset
 from .charades_cg import CharadesCGDataset
 from .charades_cd import CharadesCDDataset
 from .tacos import TACoSDataset
